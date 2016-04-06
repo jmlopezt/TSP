ba822143fc472e215e51b6f8629a4cb1a598323d
jmlopezt <juanto1514@gmail.com>
2016-04-06 15:28:52 +0200

    Example Addition

---
+++ TSP example.java
@@ -0,0 +1,39 @@
+public class Ciudad {
+	private string etiqueta;
+	private double x,y;
+}
+
+public class Problema {
+	private Ciudad[] listaCiudades;
+	private double[][] distancia;
+
+	public Problema(int n){
+
+		bla bla bla
+	}
+
+	public static Problema leerCiudades(scanner sc){
+		string etiqueta;
+		double x,y;
+
+
+		sc.next();// esto hace que nos lea hasta el espacio en blanco.
+		var= sc.nexInt;
+		for(int i= 0;i<var;i++){
+			etiqueta=sc.next();
+			x=sc.nextDouble();
+			y=sc.nextDouble();
+			ciudad=new Ciudad(eqtiqueta,x,y);
+			analdirCiudad(ciudad);
+
+		}
+	}
+
+
+public class Ruta {
+	private int[] ruta;// este metodo devuelve la ruta calculada ya, pero no el string;n sino su posicion en el vector listaciudades.
+
+
+}
+
+}
