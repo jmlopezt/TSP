0f7010f2dfad4dbb9fb24aa47efe98b057d149cc
santijr <juarezrodriguezsantiago@gmail.com>
2016-04-18 13:01:29 +0200

    Update TSP example.java

--- TSP example.java
+++ TSP example.java
@@ -10,7 +10,6 @@
 	}
 }
 
-
 public class Problema {
 	private Ciudad[] listaCiudades;
 	private double[][] distancia;
@@ -64,7 +63,6 @@ public class Problema {
 
 			
 	}
-
 
 public class Ruta {
 	
@@ -133,5 +131,46 @@ public class HeuristicaVecinoMasCercano {
 // MEJORAS PROPUESTAS
 
 //1. Estrategias de insercion
+
+public class InsercionMasEconomica{
+	private Ruta ruta;
+	//Hay que obtener una ruta inicial que es la siguiente: coordY mayor--coordX mayor--coordX menor
+	
+	public Ruta obtenerRutaInicial(ArrayList ciudades){
+		Ruta ordenViaje = new Ruta();
+		int yMax,xMax,xMin;
+		int pos1,pos2,pos3;
+		yMax=0;
+		xMax=0;
+		xMin=0;
+		//Buscamos la coordenada Y mayor.
+		for (int i=0;i<ciudades.size()-2;i++){
+			if(ciudades.get(i).getPosicion.obtenerY()>ciudades.get(i+1).getPosicion.obtenerY()){
+				yMax=ciudades.get(i).getPosicion.obtenerY();
+				pos1=i;
+			}
+		}
+		//Buscamos la coordenada X mayor.
+		for (int j=0;j<ciudades.size()-2;j++){
+			if(ciudades.get(j).getPosicion.obtenerY()>ciudades.get(j+1).getPosicion.obtenerY()){
+				yMax=ciudades.get(j).getPosicion.obtenerY();
+				pos2=j;
+			}
+		}
+		//Buscamos la coordenada X menor.
+		for (int k=0;k<ciudades.size()-2;k++){
+			if(ciudades.get(k).getPosicion.obtenerY()>ciudades.get(k+1).getPosicion.obtenerY()){
+				yMax=ciudades.get(k).getPosicion.obtenerY();
+				pos3=k;
+			}
+		}
+		
+	}
+	
+	public Ruta obtenerRutaEconomica(Ruta rutaInicial,ArrayList ciudades){
+		
+	}
+	
+}
 
 
